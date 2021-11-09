76db15d12f6b13cde549ba5af0c6ec53a733baec
Kai Xuan <kaixuan@agmostudio.com>
2021-11-09 15:06:09 +0800

    add true and false logic

--- app/src/main/java/com/example/truecitizen/MainActivity.java
+++ app/src/main/java/com/example/truecitizen/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 
 import com.example.truecitizen.databinding.ActivityMainBinding;
 import com.example.truecitizen.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -43,6 +44,28 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        binding.trueButton.setOnClickListener(v -> {
+            checkAnswer(true);
+        });
+
+        binding.falseButton.setOnClickListener(v -> {
+            checkAnswer(false);
+        });
+
+
+    }
+    private void checkAnswer(boolean userChoseCorrect){
+        boolean answerIsCorrect = questionBank[currentQuestionIndex].isAnswerTrue();
+        int messageId;
+
+        if(answerIsCorrect == userChoseCorrect){
+            messageId = R.string.correct_answer;
+        } else{
+            messageId = R.string.wrong_answer;
+        }
+
+        Snackbar.make(binding.imageView, messageId, Snackbar.LENGTH_SHORT).show();
     }
 
     private void updateQuestion() {
